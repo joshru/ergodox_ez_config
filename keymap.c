788178826477a70e8baf66d758b86f4bed97f27c
Josh Rueschenberg <joshru@uw.edu>
2020-05-07 08:45:07 -0700

    Remap caps lock to num lock

--- keymap.c
+++ keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
     KC_ESCAPE,   KC_1,    KC_2,            KC_3,    KC_4,     KC_5, KC_GRAVE,
     KC_TAB,      KC_Q,    KC_W,            KC_E,    KC_R,     KC_T, KC_DELETE,
-    KC_CAPSLOCK, KC_A,    KC_S,            KC_D,    KC_F,     KC_G,
+    KC_NLCK,     KC_A,    KC_S,            KC_D,    KC_F,     KC_G,
     KC_LSPO,     KC_Z,    KC_X,            KC_C,    KC_V,     KC_B, KC_LBRACKET,
     KC_LCTL,     KC_LALT, LALT(KC_LSHIFT), KC_LEFT, KC_RIGHT,
     
